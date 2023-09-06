7e61b8a168c3e54462220c2cede8b7d0b655ae3f
shinnosuke999 <snakeikeda0921@gmail.com>
2023-09-06 13:21:57 +0900

    Update get_cpu_temp.sh

--- sample/get_cpu_temp.sh
+++ sample/get_cpu_temp.sh
@@ -4,5 +4,8 @@ while true
 do
 #    /usr/bin/powermetrics -b 1 -n 1 -s tasks --show-process-energy --show-process-io | sed -l "s/^/$(date +%Y-%m-%dT%H:%M:%S" "%Z) /" >> ./data/process_energy_io.dat
     /usr/bin/powermetrics -b 1 -n 1 -s smc | grep "CPU Thermal level" | sed -l "s/^/$(date +%Y-%m-%dT%H:%M:%S" "%Z) /" >> ./data/cpu_thermal.dat
-    sleep 10
+    > test.txt
+    >> test.txt
+
+ sleep 10
 done
